f823f67705d26aae3d8558d2be445e9267f5ce8d
PerchunPak <git@perchun.it>
2023-02-26 18:34:25 +0100

    Add database (Redis)

--- twtb/logic/shared/db.py
+++ twtb/logic/shared/db.py
@@ -4,40 +4,51 @@ Currently, there is no need to implement it now.
 """
 import typing as t
 
+import redis.asyncio as redis
+
 import twtb.utils
 
 
 class Database(metaclass=twtb.utils.Singleton):
-    """An empty class for future database system.
+    """Main class for the data storage (aka database)."""
 
-    Todo:
-        There are only a few methods, that I can came up with now.
-        In the progress, I will add more methods, that need to be implemented.
+    _CHANNELS_KEY = ""
+    """We use disallowed symbols in Telegram nickname, so it can't be overwrote.
+
+    And for the best speed - empty string is the best.
     """
 
-    async def subscribe_user(self, user_id: int, word: str) -> None:
+    def __init__(self) -> None:
+        self._connection: redis.Redis[bytes] = redis.Redis()
+
+    async def subscribe_user(self, user: str, word: str) -> None:
         """Subscribe user to the word."""
+        await self._connection.rpush(user, word)
 
-    async def add_chanel(self, id: str) -> None:
+    async def add_chanel(self, id: int) -> None:
         """Add channel to our database."""
+        await self._connection.rpush(self._CHANNELS_KEY, id)
 
-    async def get_all_channels(self) -> t.List[str]:  # type: ignore[empty-body]
+    async def get_all_channels(self) -> t.List[int]:
         """Get all channels from database."""
+        return list(map(lambda e: int(e.decode()), await self._connection.lrange(self._CHANNELS_KEY, 0, -1)))
 
-    async def get_user_subscribes(self, user_id: int) -> t.List[str]:  # type: ignore[empty-body]
+    async def get_user_words(self, user_id: int) -> t.List[str]:
         """Get all words, which the user is subscribed to."""
+        return t.cast(t.List[str], list(map(lambda e: e.decode(), await self._connection.lrange(str(user_id), 0, -1))))
 
-    async def get_all_subscribed_words(self) -> t.Dict[str, t.List[str]]:
+    async def get_all_subscribed_words(self) -> t.Dict[str, t.List[int]]:
         """Get all words, that we need to listen.
 
         Returns:
             A :class:`dict` where key is a word to listen, and value - a list of users to forward message.
         """
-        user_to_words: t.Dict[str, t.Awaitable[t.List[str]]] = {  # type: ignore[var-annotated]
-            user_id: self.get_user_subscribes(user_id) for user_id in []  # TODO db logic
+        user_to_words: t.Dict[int, t.Awaitable[t.List[str]]] = {
+            (decoded_id := int(user_id.decode())): self.get_user_words(decoded_id)
+            for user_id in await self._connection.keys(f"[^{self._CHANNELS_KEY}]*")
         }
 
-        result: t.Dict[str, t.List[str]] = {}
+        result: t.Dict[str, t.List[int]] = {}
 
         for user_to_send, get_their_words in user_to_words.items():
             for word in await get_their_words:
