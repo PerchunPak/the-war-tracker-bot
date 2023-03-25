d0a988ee3c540f856fbed49b882690e5056a165c
PerchunPak <git@perchun.it>
2023-03-25 10:07:04 +0100

    Move users to its prefix in database

--- twtb/logic/shared/db.py
+++ twtb/logic/shared/db.py
@@ -16,47 +16,45 @@ import twtb.utils
 class Database(metaclass=twtb.utils.Singleton):
     """Main class for the data storage (aka database)."""
 
-    _CHANNELS_KEY = ""
-    """We use disallowed symbols in Telegram nickname, so it can't be overwrote.
-
-    And for the best speed - empty string is the best.
-    """
-
     def __init__(self) -> None:
         logger.info("Starting database")
         self._config = twtb.config.Config()
         self._connection: redis.Redis[bytes] = redis.Redis(
-            host=self._config.db.host, port=self._config.db.port, password=self._config.db.password
+            host=self._config.db.host,
+            port=self._config.db.port,
+            password=self._config.db.password,
         )
 
         asyncio.ensure_future(self._connection.ping())
 
-    async def subscribe_user(self, user: str, word: str) -> None:
+    async def subscribe_user(self, user: int, word: str) -> None:
         """Subscribe user to the word."""
         logger.debug(f"Subscribing {user} to {word=}")
-        await self._connection.sadd(user, word)
+        await self._connection.sadd(f"user_words:{user}", word)
 
-    async def unsubscribe_user(self, user: str, word: str) -> bool:
+    async def unsubscribe_user(self, user: int, word: str) -> bool:
         """Unsubscribe user from the word.
 
         Returns:
             Whether word was removed.
         """
         logger.debug(f"Unsubscribing {user} from {word=}")
-        return bool(await self._connection.srem(user, 0, word))
+        return bool(await self._connection.srem(f"user_words:{user}", 0, word))
 
     async def add_channel(self, id: int) -> None:
         """Add channel to our database."""
         logger.info(f"Adding channel {id} to our database")
-        await self._connection.sadd(self._CHANNELS_KEY, id)
+        await self._connection.sadd("channels", id)
 
     async def get_all_channels(self) -> t.List[int]:
         """Get all channels from database."""
-        return list(map(lambda e: int(e.decode()), await self._connection.smembers(self._CHANNELS_KEY)))
+        return list(map(lambda e: int(e.decode()), await self._connection.smembers("channels")))
 
     async def get_user_words(self, user_id: int) -> t.List[str]:
         """Get all words, which the user is subscribed to."""
-        return t.cast(t.List[str], list(map(lambda e: e.decode(), await self._connection.smembers(str(user_id)))))
+        return t.cast(
+            t.List[str], list(map(lambda e: e.decode(), await self._connection.smembers(f"user_words:{user_id}")))
+        )
 
     async def get_all_subscribed_words(self) -> t.Dict[str, t.List[int]]:
         """Get all words, that we need to listen.
@@ -67,8 +65,8 @@ class Database(metaclass=twtb.utils.Singleton):
         logger.trace("Getting all subscribed words")  # This is kind of expensive operation, so it should be logged
 
         user_to_words: t.Dict[int, t.List[str]] = {
-            (decoded_id := int(user_id.decode())): await self.get_user_words(decoded_id)
-            for user_id in await self._connection.keys(f"[^{self._CHANNELS_KEY}]*")
+            (decoded_id := int(user_id[11:].decode())): await self.get_user_words(decoded_id)
+            for user_id in await self._connection.keys("user_words:*")
         }
 
         result: t.Dict[str, t.List[int]] = {}
